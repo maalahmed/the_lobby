1df164e9f3be173821075ae1b1c985228ba4645a
maalahmed <maalahmed@gmail.com>
2025-11-08 20:26:55 +0300

    Fix null user reference in admin layout

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -141,7 +141,7 @@
                     <div class="relative">
                         <button @click="userMenuOpen = !userMenuOpen" 
                                 class="flex items-center text-sm font-medium text-gray-700 hover:text-gray-900">
-                            <span class="{{ app()->getLocale() === 'ar' ? 'ml-2' : 'mr-2' }}">{{ auth()->user()->name }}</span>
+                            <span class="{{ app()->getLocale() === 'ar' ? 'ml-2' : 'mr-2' }}">{{ auth()->user()->name ?? 'Admin' }}</span>
                             <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                                 <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"/>
                             </svg>
