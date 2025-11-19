07f5c1caedaadcc6fff3ccccda54b1b11ff86956
maalahmed <maalahmed@gmail.com>
2025-11-19 10:28:26 +0300

    message model to generate the UUID

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -5,12 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Str;
 
 class Message extends Model
 {
     use HasFactory, SoftDeletes;
 
     protected $fillable = [
+        'uuid',
         'sender_id',
         'recipient_id',
         'parent_id',
@@ -34,6 +36,17 @@ class Message extends Model
         'read_at' => 'datetime',
         'deleted_at' => 'datetime',
     ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->uuid)) {
+                $model->uuid = (string) Str::uuid();
+            }
+        });
+    }
 
     /**
      * Get the sender of the message.
